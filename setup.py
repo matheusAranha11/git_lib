0cdde2800a8140bb81a5d403044d7a348c63ce13
matheusAranha11 <103074624+matheusAranha11@users.noreply.github.com>
2022-06-02 17:44:03 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages, setup
 
 
 setup(
-      name='etl_lib',
-      packages=find_packages(include=['etl_lib']),
-      version='0.1.14',
-      description='Agrupa funções de rotinas de extração, tratamento e armazenamento de dados. ',
+      name='git_lib',
+      packages=find_packages(include=['git_lib']),
+      version='0.1.0',
+      description='Test for the usage of our libs within github',
       url='git@github.com:matheusAranha11/git_lib.git',
-      author='Victor Marchiori, Matheus Lopes',
+      author='Matheus Lopes',
       license='MIT',
       install_requires=[],
       zip_safe=False
