261f54fd3d0108a31fcd6dd4d6a0083ea963e868
Matheus Lopes <matheus.lopes@augme.local>
2022-06-02 18:25:10 -0300

    Changed setup version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
       name='git_lib',
       packages=find_packages(include=['git_lib']),
-      version='0.1.0',
+      version='0.1.1',
       description='Test for the usage of our libs within github',
       url='git@github.com:matheusAranha11/git_lib.git',
       author='Matheus Lopes',
